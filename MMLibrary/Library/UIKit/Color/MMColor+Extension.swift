126782cd2e7d493626954b6588037ecd61663426
zengliangmin <415073783@qq.com>
2019-09-23 17:23:45 +0800

    自定义颜色类

---
+++ MMLibrary/Library/UIKit/Color/MMColor+Extension.swift
@@ -0,0 +1,80 @@
+//
+//  MMColor+Extension.swift
+//  MMLibrary
+//
+//  Created by zlm on 2019/9/23.
+//  Copyright © 2019 zlm. All rights reserved.
+//
+
+import Foundation
+public extension MMColor {
+    
+//    class var red: MMColor {
+//        return MMColor(auto: 1, green: 0, blue: 0)
+//    }
+    
+    
+    override class var black: UIColor {
+        return MMColor(auto: 0, green: 0, blue: 0)
+    } // 0.0 white
+
+    override class var darkGray: UIColor {
+        return MMColor(auto: Int(0.333 * 255), green: Int(0.333 * 255), blue: Int(0.333 * 255))
+    } // 0.333 white
+
+    override class var lightGray: UIColor {
+        return MMColor(auto: Int(0.667 * 255), green: Int(0.667 * 255), blue: Int(0.667 * 255))
+    } // 0.667 white
+
+    override class var white: UIColor {
+        return MMColor(auto: 255, green: 255, blue: 255)
+    } // 1.0 white
+
+    override class var gray: UIColor {
+        return MMColor(auto: 127, green: 127, blue: 127)
+    } // 0.5 white
+
+    override class var red: UIColor {
+        return MMColor(auto: 255, green: 0, blue: 0)
+    } // 1.0, 0.0, 0.0 RGB
+
+    override class var green: UIColor {
+        return MMColor(auto: 0, green: 255, blue: 0)
+    } // 0.0, 1.0, 0.0 RGB
+
+    override class var blue: UIColor {
+        return MMColor(auto: 0, green: 0, blue: 255)
+    } // 0.0, 0.0, 1.0 RGB
+
+    override class var cyan: UIColor {
+        return MMColor(auto: 0, green: 255, blue: 255)
+    } // 0.0, 1.0, 1.0 RGB
+
+    override class var yellow: UIColor {
+        return MMColor(auto: 255, green: 255, blue: 0)
+    } // 1.0, 1.0, 0.0 RGB
+
+    override class var magenta: UIColor {
+        return MMColor(auto: 255, green: 0, blue: 255)
+    } // 1.0, 0.0, 1.0 RGB
+
+    override class var orange: UIColor {
+        return MMColor(auto: 255, green: 127, blue: 0)
+    } // 1.0, 0.5, 0.0 RGB
+
+    override class var purple: UIColor {
+        return MMColor(auto: 127, green: 0, blue: 127)
+    } // 0.5, 0.0, 0.5 RGB
+
+    override class var brown: UIColor {
+        return MMColor(auto: 153, green: 102, blue: 51)
+    } // 0.6, 0.4, 0.2 RGB
+
+    override class var clear: UIColor {
+        return MMColor(auto: 0, green: 0, blue: 0, alpha: 0)
+    } // 0.0 white, 0.0 alpha
+
+    
+    
+    
+}
---
+++ MMLibrary/Library/UIKit/Color/MMColor.swift
@@ -0,0 +1,69 @@
+//
+//  MMColor.swift
+//  MMLibrary
+//
+//  Created by zlm on 2019/9/23.
+//  Copyright © 2019 zlm. All rights reserved.
+//
+
+import Foundation
+@objc public class MMColor: UIColor {
+    
+    @objc public convenience init(auto red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0, dynamic: Bool = true) {
+        
+        if #available(iOS 13.0, *), dynamic {
+            self.init { (trait) -> UIColor in
+//                let alpha = alpha
+                var list: [Int] = [red, green, blue]
+                
+                switch trait.userInterfaceStyle {
+                case .light:
+                    break
+                case .dark:
+                    for i in 0 ..< list.count {
+                        if list[i] > 200 {
+                            list[i] = 255 - list[i]
+                        } else if list[i] < 55 {
+                            list[i] = ((55 - list[i]) + 200) % 256
+                        } else {
+                            list[i] = (list[i] + 100) % 256
+                        }
+                    }
+                    
+                default:
+                    break
+                }
+                return UIColor(red: CGFloat(list[0]) / 255.0, green: CGFloat(list[1]) / 255.0, blue: CGFloat(list[2]) / 255.0, alpha: alpha)
+            }
+        } else {
+            // Fallback on earlier versions
+            self.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: alpha)
+        }
+    }
+
+    public class func _colorWithHex(hexColor: CLong, alpha: CGFloat, dynamic: Bool = true) -> UIColor {
+        let red = ((CGFloat)((hexColor & 0xFF0000) >> 16))
+        let green = ((CGFloat)((hexColor & 0xFF00) >> 8))
+        let blue = (CGFloat)((hexColor & 0xFF))
+        
+        return MMColor(auto: Int(red), green: Int(green), blue: Int(blue), alpha: alpha, dynamic: dynamic)
+    }
+
+    @objc public class func colorWithHex(hexColor: String, alpha: Float, dynamic: Bool = true) -> UIColor {
+        var hex = hexColor
+        if hex.hasPrefix("#") {
+            hex = hex.filter {$0 != "#"}
+        }
+        if let hexVal = Int(hex, radix: 16) {
+            return MMColor._colorWithHex(hexColor: hexVal, alpha: CGFloat(alpha), dynamic: dynamic)
+        }
+        else {
+            return UIColor.clear
+        }
+    }
+
+    
+    
+    
+    
+}
